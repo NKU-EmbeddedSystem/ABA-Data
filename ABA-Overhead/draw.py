999b805c2468882b15f6fc3c4c912b6b9257cda5
jiangzhang <jiangzhangsg@qq.com>
2020-08-29 19:42:58 +0800

    change color

--- ABA-Overhead/draw.py
+++ ABA-Overhead/draw.py
@@ -24,34 +24,34 @@ for name in names:
     plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='HST', hatch='/', tick_label=x_labels, color='white')
     plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='PST', hatch='\\', tick_label=x_labels, color='white')
     plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='Pico-ST', hatch='.', tick_label=x_labels, color='white')
-    plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='native', tick_label=x_labels, color=(95/255,198/255,201/255))
-    plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='exclusive', tick_label=x_labels, color=(6/255,223/255,6/255))
-    plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='instrument', tick_label=x_labels, color=(255/255,28/255,0/255))
-    plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='mprotect', tick_label=x_labels, color=(0/255,37/255,255/255))
+    plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='native', tick_label=x_labels, color='white')
+    plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='exclusive', tick_label=x_labels, color=(249 / 255, 196 / 255, 153 / 255))
+    plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='instrument', tick_label=x_labels, color=(204 / 255, 194 / 255, 217 / 255))
+    plt.bar(index, empty.values, width = 0.33, ec='black', ls='-', label='mprotect', tick_label=x_labels, color=(143 / 255, 200 / 255, 237 / 255))
     if i == 6:
         plt.legend()
 
-    plt.bar(index, native.values, width = 0.33, ec='black', ls='-', label='native', hatch='/', color=(95/255,198/255,201/255))
-    plt.bar(index, exclusive.values, width = 0.33, ec='black', ls='-', label='exclusive', hatch='/', bottom=native.values, color=(6/255,223/255,6/255))
-    plt.bar(index, instrument.values, width = 0.33, ec='black', ls='-', label='instrument', hatch='/', bottom=native.values+exclusive.values, tick_label=x_labels, color=(255/255,28/255,0))
+    plt.bar(index, native.values, width = 0.33, ec='black', ls='-', label='native', hatch='/', color='white')
+    plt.bar(index, exclusive.values, width = 0.33, ec='black', ls='-', label='exclusive', hatch='/', bottom=native.values, color=(249 / 255, 196 / 255, 153 / 255))
+    plt.bar(index, instrument.values, width = 0.33, ec='black', ls='-', label='instrument', hatch='/', bottom=native.values+exclusive.values, tick_label=x_labels, color=(204 / 255, 194 / 255, 217 / 255))
     
     data = pd.read_table(name + '-PST.dat',sep='\t',index_col=0)
     native = data['native']
     exclusive = data['exclusive']
     mprotect = data['mprotect']
     print(name, native.values)
-    plt.bar(index + width, mprotect.values, width = 0.33, ec='black', ls='-', label='mprotect', hatch='\\', bottom=native.values+exclusive.values, color=(0,37/255,255/255))
+    plt.bar(index + width, mprotect.values, width = 0.33, ec='black', ls='-', label='mprotect', hatch='\\', bottom=native.values+exclusive.values, color=(143 / 255, 200 / 255, 237 / 255))
     
-    plt.bar(index + width, native.values, width = 0.33, ec='black', ls='-', label='native', hatch='\\', color=(95/255,198/255,201/255))
-    plt.bar(index + width, exclusive.values, width = 0.33, ec='black', ls='-', label='exclusive', hatch='\\', bottom=native.values, color=(6/255,223/255,6/255))
+    plt.bar(index + width, native.values, width = 0.33, ec='black', ls='-', label='native', hatch='\\', color='white')
+    plt.bar(index + width, exclusive.values, width = 0.33, ec='black', ls='-', label='exclusive', hatch='\\', bottom=native.values, color=(249 / 255, 196 / 255, 153 / 255))
 
     data = pd.read_table(name + '-Pico.dat',sep='\t',index_col=0)
     native = data['native']
     exclusive = data['exclusive']
     instrument = data['instrument']
-    plt.bar(index + width * 2, native.values, width = 0.33, ec='black', ls='-', label='native', hatch='.', color=(95/255,198/255,201/255))
-    plt.bar(index + width * 2, exclusive.values, width = 0.33, ec='black', ls='-', label='exclusive', hatch='.', bottom=native.values, color=(6/255,223/255,6/255))
-    plt.bar(index + width * 2, instrument.values, width = 0.33, ec='black', ls='-', label='instrument', hatch='.', bottom=native.values+exclusive.values, tick_label=x_labels, color=(255/255,28/255,0))
+    plt.bar(index + width * 2, native.values, width = 0.33, ec='black', ls='-', label='native', hatch='.', color='white')
+    plt.bar(index + width * 2, exclusive.values, width = 0.33, ec='black', ls='-', label='exclusive', hatch='.', bottom=native.values, color=(249 / 255, 196 / 255, 153 / 255))
+    plt.bar(index + width * 2, instrument.values, width = 0.33, ec='black', ls='-', label='instrument', hatch='.', bottom=native.values+exclusive.values, tick_label=x_labels, color=(204 / 255, 194 / 255, 217 / 255))
 
     if i > 5:
         plt.xlabel('threads')
